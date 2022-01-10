6d0b264301be41b2582b1064b8f5f242d30279f6
daniellisek <lisciu99@gmail.com>
2022-01-10 22:26:31 +0100

    feat: Connecting State for Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Calculator} from './components/Calculator'
+
 
 function App() {
+  const [baseQuantity, setBaseQuantity] = useState(0);
+  const [basePower, setBasePower] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="Container">
+      <div className="Main">
+        <h1>
+          Kalkulator mocy liquidu
+        </h1>
+        <div className="ContMain">
+          <h3>Liquid bazowy</h3>
+          <Calculator 
+            ilosc="Ilość liquidu bazowego [ml]" 
+            moc="Moc liquidu bazowego [mg/ml]"
+            value={baseQuantity}
+            setValue={setBaseQuantity}
+            value1={basePower}
+            setValue1={setBasePower}
+          />
+          <h3>Liquid rozcieńczający</h3>
+          {/* <Calculator 
+            ilosc="Ilość liquidu rozcieńczającego [ml]" 
+            moc="Moc liquidu rozcieńczającego [mg/ml]"
+          />           */}
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+// jebać kurwa biedaków
